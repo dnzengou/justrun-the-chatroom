5a6e07400a8993c0094a88b41f8b57cfdd626276
dnzengou <desire.yavro@gmail.com>
2016-06-11 22:01:37 +0200

    my first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+
+// IMPORTANT LINE: Point to '/public'
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('The bot is running & listening on ' + port);
+});
